429baefe1badfe86a6f494e7dbfb9536cf5fc21f
kuwabara <vimvimvimvimvimvimvimvimvimvim@gmail.com>
2014-11-04 02:31:52 +0900

    log file

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	var logDir string
+	logDefault := "!!!log default string!!!"
+	flag.StringVar(&logDir, "log", logDefault, "log directory")
+	flag.Parse()
+
+	if logDir != logDefault {
+		f, err := os.OpenFile(logDir, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(f)
+		defer f.Close()
+	}
+
 	r := mux.NewRouter()
 	// kind: notifications, cook, etc...
 	// TODO: kind and id validate
